a0c4adec606c16fd9f973acca91c4d828deb80a5
Tuan Le <tuan.le119@t-mobile.com>
2021-05-05 18:27:21 -0500

    completed

--- src/main/java/com/tmobile/springboot/controller/HelloController.java
+++ src/main/java/com/tmobile/springboot/controller/HelloController.java
@@ -1,17 +1,24 @@
 package com.tmobile.springboot.controller;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(path = "/hello-world/{number}")
-    public String sum(@PathVariable("number") String number) throws JSONException {
-        JSONObject jo = new JSONObject();
-        jo.put("sum", Integer.parseInt(number) + 10);
-        return "welcome";
+    @RequestMapping(path = "/{number}")
+    public String sum(@PathVariable("number") Integer number) {
+        Integer sum = number + 10;
+        return "{" +
+                "\"sum\"" + ": " + sum.toString() +
+                "}";
+    }
+
+    @RequestMapping(path = "/{number1}/{number2}")
+    public String sumOfTwoNumber(@PathVariable("number1") Integer number1, @PathVariable("number2") Integer number2) {
+        Integer sum = number1 + number2;
+        return "{" +
+                "\"sum\"" + ": " + sum.toString() +
+                "}";
     }
 }
